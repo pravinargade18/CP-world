87438833597463dbc4ce9808b07959159d690845
Pravin Argade <pravinargade18@gmail.com>
2022-08-11 23:56:59 +0530

    Added new approach

--- LeetCodeProblems/BitManipulation/342_Power of Four.cpp
+++ LeetCodeProblems/BitManipulation/342_Power of Four.cpp
@@ -9,3 +9,19 @@ public:
         return false;
     }
 };
+
+
+//APPROACH 2
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+    if(n>0){
+    int setBits= __builtin_popcount(n);
+    int zeroCount=__builtin_ctz(n);     //if setbit is 1 and no of trailing zeroes are even then the number is pow of 4
+    if(setBits==1 && zeroCount%2==0){
+        return true;
+    }
+    }
+    return false;
+    }
+};
