555648d5e67a934a847110af0f73ae56725c745d
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 10:55:55 +0530

    Added another approach

--- LeetCodeProblems/HashMap/169_Majority Element.cpp
+++ LeetCodeProblems/HashMap/169_Majority Element.cpp
@@ -14,3 +14,15 @@ public:
         return -1;
     }
 };
+
+
+//approach - 2
+class Solution {
+public:
+    
+    // the number accupying more than half space must be somewhere at middle of the array
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+    }
+};
