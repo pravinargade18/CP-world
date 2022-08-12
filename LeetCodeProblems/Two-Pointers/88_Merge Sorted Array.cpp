354406fec031d17e8c3a01a4927be73cbbff445f
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 10:25:11 +0530

    Added another approach

--- LeetCodeProblems/Two-Pointers/88_Merge Sorted Array.cpp
+++ LeetCodeProblems/Two-Pointers/88_Merge Sorted Array.cpp
@@ -30,3 +30,17 @@ public:
         
     }
 };
+
+
+//approach 2
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int j=0;
+        for(int i=m;i<m+n;i++){
+            nums1[i]=nums2[j++]; //merged all elements of 2nd array into first from m index and then sort the 1st array 
+        }
+        sort(nums1.begin(),nums1.end());
+        
+    }
+};
