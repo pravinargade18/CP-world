025023e3cb5c59c9949258f3934b1b6293e62965
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 12:10:46 +0530

    Added new problem

---
+++ LeetCodeProblems/Strings/9_Palindrome_Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string rev = to_string(x);
+        reverse(rev.begin(), rev.end());
+        return to_string(x) == rev;
+        
+    }
+};
