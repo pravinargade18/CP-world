82a3fb6b5200ab01e4c83f1513b88f5e6aa3644e
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 10:38:54 +0530

    Added new problem

---
+++ LeetCodeProblems/BinarySearch/35_Search Insert Position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+        int mid=0;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return s;
+        
+    }
+};
