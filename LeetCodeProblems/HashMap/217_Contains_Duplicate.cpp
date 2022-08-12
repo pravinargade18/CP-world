40ef6bf650828c1c952ec96a592251f95c984443
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 13:59:12 +0530

    Added another approach

--- LeetCodeProblems/HashMap/217_Contains_Duplicate.cpp
+++ LeetCodeProblems/HashMap/217_Contains_Duplicate.cpp
@@ -16,3 +16,36 @@ public:
     
     }
 };
+
+
+//approach 2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        for(auto i:nums){
+            s.insert(i);
+        }
+        return s.size()!=nums.size();
+    }
+};
+
+
+//approach 3
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int j=1;
+        for(int i=0;i<nums.size()-1;i++){
+            
+                if(nums[i]==nums[j]){
+                    return true;
+                    break;
+                }
+            j++;
+            
+        }
+        return false;
+    }
+};
