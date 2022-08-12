34a2fa0f5fc47e8c335616c84f8cd103b7407ae2
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 13:54:28 +0530

    Added new problem

---
+++ LeetCodeProblems/Recursion/509_Fibonacci_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        //base case
+        if(n==0){
+            return 0;
+        }
+        //base case
+        if(n==1){
+            return 1;
+        }
+        int ans=fib(n-1)+fib(n-2);
+        return ans;
+    }
+};
