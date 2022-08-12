e1c484ea71519829cf4e83bb8bc53c347395f5a9
Pravin Argade <pravinargade18@gmail.com>
2022-08-12 11:28:23 +0530

    Added another approach

--- LeetCodeProblems/2D-Array/74_Search_a_2D_Matrix.cpp
+++ LeetCodeProblems/2D-Array/74_Search_a_2D_Matrix.cpp
@@ -48,3 +48,22 @@ public:
             return false;
     }
 };
+
+//approach 2
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int row=arr.size();
+        int col=arr[0].size();
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(arr[i][j]==target){
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+    }
+};
