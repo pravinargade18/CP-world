81a9773cb3cbeaccd09eb8e5ef7490affa8313fd
Pravin Argade <pravinargade18@gmail.com>
2022-08-19 11:16:50 +0530

    Starters 52 problem added

---
+++ CodeChef/Easy/Air_Conditioner_Temperature.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define fast_io              \
+    ios::sync_with_stdio(0); \
+    cin.tie(NULL);           \
+    cout.tie(NULL);
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define po pop_back
+
+void solve()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(b>=a && b>=c){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+
+
+int main()
+{
+    fast_io;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt",
+            "r", stdin);
+    freopen("output.txt",
+            "w", stdout);
+#endif
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
