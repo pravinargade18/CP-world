9fe93b8acd92c9f7c133d76c3717e083b278c06f
Pravin Argade <pravinargade18@gmail.com>
2022-08-26 10:44:52 +0530

    Added recursive solution

--- LeetCodeProblems/LinkedList/206_Reverse_Linked_List.cpp
+++ LeetCodeProblems/LinkedList/206_Reverse_Linked_List.cpp
@@ -27,3 +27,36 @@ public:
          
     }
 };
+
+
+// Recursive solution
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    void Reverse(ListNode *&head,ListNode* curr,ListNode *prev){
+        if(curr==NULL){
+            head=prev;
+            return;
+        }
+        ListNode* forward=curr->next;   //made link between first node and other nodes
+        Reverse(head,forward,curr);
+        curr->next=prev;                    
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+       ListNode* prev=NULL;
+        ListNode* curr=head;
+        Reverse(head,curr,prev);
+        return head;
+    }
+};
