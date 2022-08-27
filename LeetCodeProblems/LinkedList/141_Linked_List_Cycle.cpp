2f9c095643b1de02e6182df972a7e6942a738f74
Pravin Argade <pravinargade18@gmail.com>
2022-08-27 10:37:11 +0530

    Added new approach

--- LeetCodeProblems/LinkedList/141_Linked_List_Cycle.cpp
+++ LeetCodeProblems/LinkedList/141_Linked_List_Cycle.cpp
@@ -26,3 +26,34 @@ public:
         
     }
 };
+
+
+// Approach 2-using hashmap
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL){
+            return false;
+        }
+        unordered_map<ListNode*,bool>visited;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(visited[temp]==true){  // if node is already visited
+                return true;
+            }
+            visited[temp]=true; // if not visited then mark true
+            temp=temp->next;
+            
+        }
+        return false;
+        
+    }
+};
