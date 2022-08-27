84fdd954ef7d7411ffc4ca36ce1730ced7310f0e
Pravin Argade <pravinargade18@gmail.com>
2022-08-27 10:13:42 +0530

    Added new problem

---
+++ LeetCodeProblems/LinkedList/141_Linked_List_Cycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL){
+            return false;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL){   //these conditions to check that ll is circular  //if fast at last position and got next as null then no cycle and same for fast->next->next
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){          // there will be definitely one point where fast will be equal to slow if the linked list is circular
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
