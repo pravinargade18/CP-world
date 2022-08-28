254b80cb0bccf1aa1c8e1eaf9527e8f43c60f9e4
Pravin Argade <pravinargade18@gmail.com>
2022-08-28 17:31:21 +0530

    Added new approach

--- GeeksForGeeks/LinkedListProblems/Finding_middle_element_in_a_linked_list.cpp
+++ GeeksForGeeks/LinkedListProblems/Finding_middle_element_in_a_linked_list.cpp
@@ -23,3 +23,22 @@ class Solution{
         return ptr->data;
     }
 };
+
+
+// Approach 2-->Two pointer approach
+class Solution{
+    public:
+   
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+       Node *slow=head;
+       Node *fast=head;
+       
+       while(fast!=NULL && fast->next!=NULL){
+           slow=slow->next;
+           fast=fast->next->next;
+       }
+       return slow->data;
+    }
+};
