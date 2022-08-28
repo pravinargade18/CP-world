ffef75322f9df4afdae0297f82d8c71b3cbae132
Pravin Argade <pravinargade18@gmail.com>
2022-08-28 15:55:16 +0530

    added another approach

--- GeeksForGeeks/LinkedListProblems/Detect_Loop_in_linked_list.cpp
+++ GeeksForGeeks/LinkedListProblems/Detect_Loop_in_linked_list.cpp
@@ -24,3 +24,32 @@ class Solution
         
     }
 };
+
+// Approach 2-->using unordered map
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        if(head==NULL){
+            return false;
+        }
+        // your code here
+       unordered_map<Node*,bool>mp;
+       Node* temp=head;
+       while(temp!=NULL){
+           if(mp[temp]==true){
+               return true;
+           }
+           else{
+               mp[temp]=true;
+           }
+           temp=temp->next;
+           
+       }
+       return false;
+        
+        
+    }
+};
