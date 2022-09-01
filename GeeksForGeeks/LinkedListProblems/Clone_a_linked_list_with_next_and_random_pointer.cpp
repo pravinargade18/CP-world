693d855d605d5f5c7c6150d917583cb9e21f80e4
Pravin Argade <pravinargade18@gmail.com>
2022-09-01 23:17:11 +0530

    Added another approach

--- GeeksForGeeks/LinkedListProblems/Clone_a_linked_list_with_next_and_random_pointer.cpp
+++ GeeksForGeeks/LinkedListProblems/Clone_a_linked_list_with_next_and_random_pointer.cpp
@@ -46,3 +46,55 @@ class Solution
     }
 
 };
+
+
+// Optimised approach without using map
+
+class Solution
+{
+    private:
+        void insertAtTail(Node* &head,Node* &tail,int data){
+            Node* newNode=new Node(data);
+            if(head==NULL){
+                head=newNode;
+                tail=newNode;
+            }
+            else{
+                tail->next=newNode;
+                tail=newNode;
+            }
+        }
+    public:
+    Node *copyList(Node *head)
+    {
+         Node* cloneHead=NULL;
+         Node* cloneTail=NULL;
+         
+         Node* temp=head;
+         while(temp!=NULL){
+             insertAtTail(cloneHead,cloneTail,temp->data);
+             temp=temp->next;
+         }
+         
+         Node* originalNode=head;
+         Node* cloneNode=cloneHead;
+         
+         unordered_map<Node*,Node*> mp;
+         while(originalNode!=NULL){
+             mp[originalNode]=cloneNode;
+             originalNode=originalNode->next;
+             cloneNode=cloneNode->next;
+         }
+         
+         originalNode=head;
+         cloneNode=cloneHead;
+         while(originalNode!=NULL){
+             cloneNode->arb=mp[originalNode->arb];
+             originalNode=originalNode->next;
+             cloneNode=cloneNode->next;
+         }
+         
+         return cloneHead;
+    }
+
+};
