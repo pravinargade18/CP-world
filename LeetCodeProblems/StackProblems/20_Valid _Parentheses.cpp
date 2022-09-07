b405a8fabaa958a44f252554f6a9518b23548310
Pravin Argade <pravinargade18@gmail.com>
2022-09-08 00:16:12 +0530

    Added new problem

---
+++ LeetCodeProblems/StackProblems/20_Valid _Parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(!st.empty()){
+                    char top=st.top();
+                    if((ch==')' && top=='(') || (ch==']' && top=='[') || (ch=='}' && top=='{')){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                     }
+                }
+                else{
+                    return false;
+                }
+            }
+            
+        }
+        return st.empty()?true:false;
+    }
+};
