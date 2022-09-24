d4e81c05a9eb34b1d4f59a3b32cac36e5e5c4c26
Pravin Argade <pravinargade18@gmail.com>
2022-09-24 19:00:12 +0530

    Added another approach

--- GeeksForGeeks/QueueProblems/Queue_Reversal.cpp
+++ GeeksForGeeks/QueueProblems/Queue_Reversal.cpp
@@ -1,3 +1,4 @@
+// Approach 1-->using recursion
 void reverseQueue(queue<int> &q){
     if(q.empty()){
         return;
@@ -11,5 +12,25 @@ queue<int> rev(queue<int> q)
 {
     
     reverseQueue(q);
+    return q;
+}
+
+
+
+// Approach 2-->using stack
+
+queue<int> rev(queue<int> q)
+{
+    stack<int>st;
+    while(!q.empty()){
+        int val=q.front();
+        st.push(val);
+        q.pop();
+    }
+    while(!st.empty()){
+        int val=st.top();
+        q.push(val);
+        st.pop();
+    }
     return q;
 }
