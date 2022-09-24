654e99ed4cde605bc2f795a49241f03f4b2bfb65
Pravin Argade <pravinargade18@gmail.com>
2022-09-25 01:11:58 +0530

    Added updated code

--- LeetCodeProblems/Arrays/912_Sort_an_Array.cpp
+++ LeetCodeProblems/Arrays/912_Sort_an_Array.cpp
@@ -1,6 +1,62 @@
+// Approach 1
+class Solution {
+private:
+    void merge(vector<int>& nums,int s,int e){
+        int mid=s+(e-s)/2;
+        int len1=mid-s+1;
+        int len2=e-mid;
+        
+        int *first=new int[len1];
+        int *second=new int[len2];
+        
+        
+        int k=s;
+        for(int i=0;i<len1;i++){
+            first[i]=nums[k++];
+        }
+        k=mid+1;
+        for(int i=0;i<len2;i++){
+            second[i]=nums[k++];
+        }
+        int index1=0,index2=0;
+        k=s;
+        while(index1<len1 && index2< len2){
+            if(first[index1]<second[index2]){
+                nums[k++]=first[index1++];
+            }
+            else{
+               nums[k++]=second[index2++]; 
+            }
+        }
+        while(index1<len1){
+            nums[k++]=first[index1++];
+        }
+        while(index2<len2){
+            nums[k++]=second[index2++];
+        }
+        delete [] first;
+        delete []  second;
+    }
+     void mergeSort(vector<int>& nums,int s,int e){
+        if(s>=e){
+            return ;
+        }
+        int mid=s+(e-s)/2;
+        mergeSort(nums,s,mid);
+        mergeSort(nums,mid+1,e);
+        merge(nums,s,e);
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
+    }
+};
 
 
+    
 
+// Approach 2-->another merging approach
 class Solution {
 private:
     void merge(vector<int>& nums,int s,int e){
