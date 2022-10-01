9b1dee72b7817861d1f981821ab900de67d2abf0
Pravin Argade <pravinargade18@gmail.com>
2022-10-01 20:04:37 +0530

    Added another approach

--- LeetCodeProblems/LinkedList/160_Intersection_of_Two_Linked_Lists.cpp
+++ LeetCodeProblems/LinkedList/160_Intersection_of_Two_Linked_Lists.cpp
@@ -29,3 +29,51 @@ public:
         // return b;
     }
 };
+
+
+// another approach
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int getLength(ListNode* head){
+        int len=0;
+        while(head!=NULL){
+            len++;
+            head=head->next;
+        }
+        return len;
+    }
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a=headA;
+        ListNode* b=headB;
+        int lenA=getLength(headA);
+        int lenB=getLength(headB);
+        if(lenB>lenA){
+            int len=lenB-lenA;
+            while(len--){
+                b=b->next;
+            }
+                
+        }
+         if(lenA>lenB){
+            int len=lenA-lenB;
+            while(len--){
+                a=a->next;
+            }
+                
+        }
+        while(a!=b){
+            a=a->next;
+            b=b->next;
+        }
+        return a;
+    }
+};
