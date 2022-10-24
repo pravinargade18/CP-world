3d2d6916011f86cfddce7bc7f360292d4a3909f5
Pravin Argade <pravinargade18@gmail.com>
2022-10-24 20:34:53 +0530

    Added another traversal

--- CodeStudio/Tree_Problems/Count_leaf_nodes.cpp
+++ CodeStudio/Tree_Problems/Count_leaf_nodes.cpp
@@ -33,3 +33,41 @@ int noOfLeafNodes(BinaryTreeNode<int> *root){
     postorder(root,count);
     return count;
 }
+
+
+//using preorder traversal
+
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+    
+***********************************************************/
+void preorder(BinaryTreeNode<int> *root,int &count){
+    if(root==NULL){
+        return;
+    }
+    if(root->left==NULL && root->right==NULL){
+        count++;
+    }
+    preorder(root->left,count);
+    preorder(root->right,count);
+}
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    int count=0;
+    preorder(root,count);
+    return count;
+}
