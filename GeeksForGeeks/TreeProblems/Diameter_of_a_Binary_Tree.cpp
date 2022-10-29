2801509515316f48e370483139501951e3746411
Pravin Argade <pravinargade18@gmail.com>
2022-10-29 11:07:54 +0530

    Added another approach

--- GeeksForGeeks/TreeProblems/Diameter_of_a_Binary_Tree.cpp
+++ GeeksForGeeks/TreeProblems/Diameter_of_a_Binary_Tree.cpp
@@ -37,4 +37,43 @@ class Solution {
     }
 };
 
+//Approach 2-->without height function instead with the help of pair
 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  private:
+    pair<int,int>diameterFast(Node* root){
+        if(root==NULL){
+            pair<int,int>p=make_pair(0,0);
+            return p;
+        }
+       pair<int,int> left= diameterFast(root->left);
+
+        pair<int,int> right = diameterFast(root->right);
+        
+        int op1=left.first;
+        int op2=right.first;
+        int op3=left.second+right.second+1;
+        
+        pair<int,int>ans;
+        ans.first=max(op1,max(op2,op3));
+        ans.second=max(left.second,right.second)+1;
+        return ans;
+    }
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        return diameterFast(root).first;
+    }
+};
