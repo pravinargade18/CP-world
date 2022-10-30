dabb58dc0fd557fc45505d08fc34ac23bb7194ba
Pravin Argade <pravinargade18@gmail.com>
2022-10-30 23:04:34 +0530

    Added optimised approach

--- LeetCodeProblems/TreeProblems/543_Diameter_of_Binary_Tree.cpp
+++ LeetCodeProblems/TreeProblems/543_Diameter_of_Binary_Tree.cpp
@@ -33,3 +33,44 @@ public:
         return ans;
     }
 };
+
+
+//Approach 2-->Optimised approach using pair
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    pair<int,int> diameterFast(TreeNode* root){
+            if(root==NULL){
+                pair<int,int>p=make_pair(0,0);
+                return p;
+            }
+            pair<int,int> left=diameterFast(root->left);
+            pair<int,int> right=diameterFast(root->right);
+            
+            int op1=left.first;
+            int op2=right.first;
+            int op3=left.second+right.second;
+        
+            pair<int,int>ans;
+        
+            ans.first=max(op1,max(op2,op3));
+            ans.second=max(left.second,right.second)+1;
+            return ans;
+        }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+       
+        return diameterFast(root).first;
+    }
+};
