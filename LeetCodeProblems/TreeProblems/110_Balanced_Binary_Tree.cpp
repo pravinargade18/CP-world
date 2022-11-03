1b2ad5cc09fa19863e69ebe5fc7d90dd680a4981
Pravin Argade <pravinargade18@gmail.com>
2022-11-03 22:23:46 +0530

    Added optimised approach

--- LeetCodeProblems/TreeProblems/110_Balanced_Binary_Tree.cpp
+++ LeetCodeProblems/TreeProblems/110_Balanced_Binary_Tree.cpp
@@ -41,3 +41,48 @@ public:
     }
 };
 
+
+// Added another approach
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+   pair<bool,int>isBalancedFast(TreeNode* root){
+            if(root==NULL){
+                pair<bool,int>p=make_pair(true,0);
+                return p;
+            }
+        
+        pair<bool,int> left=isBalancedFast(root->left);
+        pair<bool,int> right=isBalancedFast(root->right);
+        
+        bool leftAns=left.first;
+        bool rightAns=right.first;
+        bool diff=abs(left.second-right.second)<=1;
+        
+        pair<bool,int>ans;
+        ans.second=max(left.second,right.second)+1;
+        if(leftAns && rightAns && diff){
+            ans.first= true;
+        }
+        else{
+            ans.first= false;
+        }
+        return ans;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+       return isBalancedFast(root).first;
+        }
+};
+
