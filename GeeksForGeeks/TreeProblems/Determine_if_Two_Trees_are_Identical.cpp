a04021a9ecd247e9c148a9e15a228043bea1a40b
Pravin Argade <pravinargade18@gmail.com>
2022-11-05 23:31:38 +0530

    Added new problem

---
+++ GeeksForGeeks/TreeProblems/Determine_if_Two_Trees_are_Identical.cpp
@@ -0,0 +1,48 @@
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+   
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        if(r1==NULL && r2==NULL){
+            return true;
+        }
+        
+        if(r1==NULL && r2!=NULL){
+            return false;
+        }
+        
+        if(r1!=NULL && r2==NULL){
+            return false;
+        }
+        
+        bool left=isIdentical(r1->left,r2->left);
+        bool right=isIdentical(r1->right,r2->right);
+        
+        bool data=r1->data==r2->data;
+        
+        if(left && right && data){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
