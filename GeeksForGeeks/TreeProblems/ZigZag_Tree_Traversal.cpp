524d12eff3f64d3e8424fa0f642cb6a33fd157bb
Pravin Argade <pravinargade18@gmail.com>
2022-11-07 08:11:34 +0530

    Added new problem

---
+++ GeeksForGeeks/TreeProblems/ZigZag_Tree_Traversal.cpp
@@ -0,0 +1,58 @@
+/*Structure of the node of the binary tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<int>result;
+    	if(root==NULL){
+    	    return result;
+    	}
+    	
+    	queue<Node*>q;
+    	q.push(root);
+    	
+    	bool leftToRight=true;
+    	
+    	while(!q.empty()){
+    	    int size=q.size();
+    	    vector<int>ans(size);
+    	    
+    	    for(int i=0;i<size;i++){
+    	         Node* frontNode=q.front();
+    	         q.pop();
+    	         
+    	         int index=leftToRight?i:size-i-1;
+    	         ans[index]=frontNode->data;
+    	         
+    	         if(frontNode->left){
+    	             q.push(frontNode->left);
+    	         }
+    	         
+    	         if(frontNode->right){
+    	             q.push(frontNode->right);
+    	         }
+    	    }
+    	    
+    	    leftToRight=!leftToRight;
+    	    for(auto i:ans){
+    	        result.push_back(i);
+        	}
+    	}
+    	
+    	return result;
+    	
+    }
+};
