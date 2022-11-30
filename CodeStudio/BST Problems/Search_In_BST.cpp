b3c91b5d53dbeaf421bb6413bdaaa28e3308d5ca
Pravin Argade <pravinargade18@gmail.com>
2022-11-30 09:13:26 +0530

    Added another approach

--- CodeStudio/BST Problems/Search_In_BST.cpp
+++ CodeStudio/BST Problems/Search_In_BST.cpp
@@ -33,3 +33,40 @@ bool searchInBST(BinaryTreeNode<int> *root, int x) {
     return false;
         
 }
+
+//another approach
+
+#include <bits/stdc++.h> 
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+   if(root==NULL){
+            return false;
+        }
+        
+        if(x==root->data){
+            return true;
+        }
+        
+        if(x<root->data){
+            searchInBST(root->left,x);
+        }
+        else{
+            searchInBST(root->right,x);
+        }
+}
