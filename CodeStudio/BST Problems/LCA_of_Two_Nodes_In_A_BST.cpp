7c6df99b0041d997d9d05e8bb8f7b0e4e2423b80
Pravin Argade <pravinargade18@gmail.com>
2022-12-29 22:30:26 +0530

    Added new problem

---
+++ CodeStudio/BST Problems/LCA_of_Two_Nodes_In_A_BST.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h> 
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+
+TreeNode<int>* LCAinaBST(TreeNode<int>* root, TreeNode<int>* P, TreeNode<int>* Q)
+{
+	if(root==NULL){
+        return NULL;
+    }
+    
+    if(P->data<root->data && Q->data <root->data){
+        return LCAinaBST(root->left,P,Q);
+    }
+    
+    if(P->data>root->data && Q->data >root->data){
+        return LCAinaBST(root->right,P,Q);
+    }
+    
+    else{
+        return root;
+    }
+    
+}
