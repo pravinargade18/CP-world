583c904d1ffe6b0590b5d7503b564e7043f7f920
Pravin Argade <pravinargade18@gmail.com>
2023-01-05 10:16:30 +0530

    Added new problem

---
+++ LeetCodeProblems/TreeProblems/590_N-ary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void solve(Node* root,vector<int>& ans){
+        if(root==NULL){
+            return ;
+        }
+        for(auto i:root->children){
+            solve(i,ans);
+        }
+        ans.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+    }
+};
