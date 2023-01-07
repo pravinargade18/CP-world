4e2543a2cab0e36607f3d60e2c23e18b9ec12531
Pravin Argade <pravinargade18@gmail.com>
2023-01-07 21:19:36 +0530

    Added new problem

---
+++ LeetCodeProblems/BST Problems/222_Count_Complete_Tree_Nodes.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,int& cnt){
+        if(root==NULL){
+            return ;
+        }
+        inorder(root->left,cnt);
+        cnt++;
+        inorder(root->right,cnt);
+    }
+    int countNodes(TreeNode* root) {
+        int cnt=0;
+        inorder(root,cnt);
+        return cnt;
+    }
+};
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   
+    int countNodes(TreeNode* root) {
+        return root==NULL?0:countNodes(root->left)+countNodes(root->right)+1;
+    }
+};
