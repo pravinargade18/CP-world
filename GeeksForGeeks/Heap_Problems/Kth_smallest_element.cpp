32a99dcaf918885e0c475d9eb76ce1ffaab5db41
Pravin Argade <pravinargade18@gmail.com>
2023-01-07 21:29:58 +0530

    Added new problem

---
+++ GeeksForGeeks/Heap_Problems/Kth_smallest_element.cpp
@@ -0,0 +1,23 @@
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        priority_queue<int>pq;
+        for(int i=0;i<=r;i++){
+            pq.push(arr[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        
+        
+        
+        return pq.top();
+    }
+};
