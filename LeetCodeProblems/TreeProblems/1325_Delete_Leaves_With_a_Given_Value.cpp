8ff7dd02636ff02066bbdedc31f49c1a874362d3
Pravin Argade <pravinargade18@gmail.com>
2023-01-15 22:02:16 +0530

    Added new problem

---
+++ LeetCodeProblems/TreeProblems/1325_Delete_Leaves_With_a_Given_Value.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(root==NULL){
+            return NULL;
+        }
+        root->left=removeLeafNodes(root->left,target);
+        root->right=removeLeafNodes(root->right,target);
+        
+        if(root->left==NULL && root->right==NULL){
+            if(root->val==target){
+               return NULL;
+            }
+        }
+        return root;
+    }
+};
