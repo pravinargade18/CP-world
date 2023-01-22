ffe3d039a953597bb3ef1cfd021436236d78ae62
Pravin Argade <pravinargade18@gmail.com>
2023-01-22 19:25:50 +0530

    Added new problem

--- LeetCodeProblems/TreeProblems/129_Sum_Root_to_Leaf_Numbers.cpp
+++ LeetCodeProblems/TreeProblems/129_Sum_Root_to_Leaf_Numbers.cpp
@@ -30,3 +30,33 @@ public:
         return solve(root,s);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root,int sum){
+        if(root==NULL){
+            return 0;
+        }
+        sum=sum*10+root->val;
+        if(!root->left && !root->right){
+            return sum;
+        }
+        return solve(root->left,sum)+solve(root->right,sum);
+        
+    }
+    int sumNumbers(TreeNode* root) {
+        int sum=0;
+        return solve(root,sum);
+    }
+};
