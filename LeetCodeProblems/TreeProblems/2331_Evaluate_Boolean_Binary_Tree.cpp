00cdff50cb2e01afc5ff6bb19e105c2161dbca4f
Pravin Argade <pravinargade18@gmail.com>
2023-02-28 21:03:43 +0530

    Added new problem

---
+++ LeetCodeProblems/TreeProblems/2331_Evaluate_Boolean_Binary_Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans;
+    bool evaluateTree(TreeNode* root) {
+        if(root->val==1 || root->val==0){
+            return root->val;
+        }
+        bool left=evaluateTree(root->left);
+        bool right=evaluateTree(root->right);
+        if(root->val==2){
+            return left||right;
+        }
+        else{
+            return left&&right;
+        }
+        
+    }
+};
