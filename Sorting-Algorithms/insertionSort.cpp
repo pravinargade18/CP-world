46bdc2ca9bf1bc01c2b6addf2dda709c67782f61
Pravin Argade <pravinargade18@gmail.com>
2023-08-22 19:30:55 +0530

    Update insertionSort.cpp

--- Sorting-Algorithms/insertionSort.cpp
+++ Sorting-Algorithms/insertionSort.cpp
@@ -1,23 +1,18 @@
 #include<vector>
 using namespace std;
 
-void insertionSort(int n, vector<int> &arr){
-    
-    for(int i = 1; i<n; i++) {
-        int temp = arr[i];
-        int j = i-1;
-        for(; j>=0; j--) {
-            
-            if(arr[j] > temp) {
-                //shift
-                arr[j+1] = arr[j];
-            }
-            else { // ruk jao
-                break;
-            }
-            
+int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+ 
+        // Move elements of arr[0..i-1],
+        // that are greater than key,
+        // to one position ahead of their
+        // current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        //copy temp value
-        arr[j+1] = temp;  
-    } 
-}
+        arr[j + 1] = key;
+ }
